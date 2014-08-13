d12fb23798d6de09c49ce7c70c0a9113babb4935
0xFFF0007 <JeskeCode@users.noreply.github.com>
2014-08-13 12:20:59 -0400

    Update One-LinePS.ps1

--- One-LinePS.ps1
+++ One-LinePS.ps1
@@ -1,2 +1,4 @@
 #Dump all Computers form AD
 #Get-ADComputer -Filter * -Property * | Select-Object Name,OperatingSystem,OperatingSystemServicePack,OperatingSystemVersion | Export-CSV AllWindows.csv -NoTypeInformation -Encoding UTF8
+
+#Get-ADComputer -Filter { OperatingSystem -Like '*Windows 2000 Server*' } -Properties OperatingSystem | Select Name, OperatingSystem, DistinguishedName, Enabled | Format-Table -AutoSize
